5d603018955f944bf5a4feca0fafaf1367bc5cd2
mrhy <louis.guerout@ynov.com>
2025-11-18 15:40:29 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+// Page d'accueil
+func homePage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		r.ParseForm()
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	http.ServeFile(w, r, "./templates/acceuil.html")
+}
+
+func main() {
+	// Routes HTML
+	// la racine (/) sert la page d'accueil ; garder le chemin explicite pour compatibilité
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/acceuil.html", homePage)
+
+	// Fichiers statiques (servir depuis le dossier page_web)
+	http.Handle("/style.css", http.FileServer(http.Dir("./static/css")))
+	http.Handle("/script.js", http.FileServer(http.Dir("./static/script")))
+	// Images et autres ressources statiques
+	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./static/images"))))
+
+	// Permet de remplacer le port via la variable d'environnement PORT (utile pour l'hébergement ou éviter les conflits)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5500"
+	}
+
+	fmt.Printf("Serveur démarré sur http://localhost:%s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
