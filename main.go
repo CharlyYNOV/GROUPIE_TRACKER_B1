439c6c51e11c1017a281fabd3e15d8d6ba69372d
mrhy-s <louis.guerout@ynov.com>
2025-12-01 18:14:33 +0100

    Added new route in order to see viewAllArtists.html
    Duplicating route "homePage" to create "viewAllArtistsPage"

--- main.go
+++ main.go
@@ -50,13 +50,38 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Nouvelle route pour la page des Artistes
+func viewAllArtistsPage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		r.ParseForm()
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("./templates/viewAllArtists.html")
+	if err != nil {
+		log.Printf("Error parsing template: %v", err)
+		http.Error(w, "Error while loading page", http.StatusInternalServerError)
+		return
+	}
+
+	data := struct{ Artists []internals.Artist }{Artists: internals.Artists}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Printf("Error executing template: %v", err)
+		http.Error(w, "Error while displaying page", http.StatusInternalServerError)
+		return
+	}
+}
+
 func main() {
 	internals.Main_api()
 
 	// Routes HTML
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/acceuil.html", homePage)
-	http.HandleFunc("/templates/viewAllArtists.html", homePage)
+	http.HandleFunc("/artists", viewAllArtistsPage)
 
 	// Fichiers statiques
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./static/css"))))
